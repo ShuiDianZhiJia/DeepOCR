f7a12819c9196bd0a174a3af31e555969d7f0e5e
yubb <2601169057@qq.com>
2018-04-19 21:12:25 +0800

    error

--- src/core/scanner.py
+++ src/core/scanner.py
@@ -47,7 +47,7 @@ class Scanner:
 
 
 def split_hor_edge():
-    img1 = cv2.imread('./timg.jpg', 0)
+    img1 = cv2.imread('./exam.png', 0)
     ret, img = cv2.threshold(img1, 127, 255, cv2.THRESH_BINARY)
     # ver_edge = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
     hor_edge = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
@@ -87,9 +87,23 @@ def split_hor_edge():
     return h_rs
 
 
-def split_vor_edge(path='./tmp/21.png'):
-    img = cv2.imread(path, 0)
-    # ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+def max_distance(cols):
+    max_d = 0
+    pre_max = 0
+    for idx in range(len(cols)):
+        if idx + 1 < len(cols):
+            if cols[idx + 1] - cols[idx] == 1:
+                max_d += 1
+            else:
+                pre_max = max_d if max_d > pre_max else pre_max
+                max_d = 0
+    return pre_max
+
+
+def split_vor_edge(path='./tmp/112.png'):
+    img1 = cv2.imread(path, 0)
+    ret, img = cv2.threshold(img1, 125, 255, cv2.THRESH_BINARY)
+    cv2.imwrite('./tmp/rs/bin.png', img)
     ver_edge = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
     height, width = img.shape
     x_axis, y_axis, x_span, y_span, num = 0, 0, 3, 3, 0
@@ -110,19 +124,41 @@ def split_vor_edge(path='./tmp/21.png'):
 
     cv2.imwrite('./tmp/rs/v.png', v_rs)
     h, w = v_rs.shape
-    cur_is_having, pre_is_having, pre_idx = False, False, 0
-    for w_ in range(w):
-        line = v_rs[:, w_]
-        cur_is_having = len(np.where(line != 0)[0]) > 0
-        if cur_is_having:
-            if not pre_is_having:
-                pre_idx = w_
+    cur_is_having, pre_is_having, begin = False, False, 0
+
+    # col_map = np.ones(w) * 255
+    map = None
+    for idx in range(w):
+        col = img[:, idx]
+        arr = np.where(col == 0)[0]
+        col = col.reshape(len(col), 1)
+        if len(arr):
+            col[arr[0]:] = 0
+        if map is None:
+            map = col
         else:
-            if pre_is_having:
-                cv2.imwrite('./tmp/rs/{}.png'.format(w_), img[:, pre_idx:w_])
-                pre_idx = w_
+            map = np.hstack((map, col))
+    cv2.imwrite('./tmp/rs/map.png', map)
+    # for x in cols:
+    #     col_map[x] = 0
+    # cv2.imwrite('./tmp/rs/map.png', col_map.reshape(1, w))
+    # print(col_map)
+
+    for w_ in range(w):
+        cur_is_having = len(np.where(v_rs[:, w_] != 0)[0]) > 0
+
+        # 字符开始
+        if cur_is_having and not pre_is_having:
+            if w_ - begin >= 20:
+                begin = w_
+
+        # 字符结束
+        if pre_is_having and not cur_is_having:
+            if w_ - begin >= 17:
+                cv2.imwrite('./tmp/rs/{}.png'.format(w_), img1[:, begin:w_])
         pre_is_having = cur_is_having
 
 
+np.set_printoptions(threshold=np.nan)
 # split_hor_edge()
 split_vor_edge()
